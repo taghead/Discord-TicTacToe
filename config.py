c4c2c96008af7f47226ee5adfa6b8444be625b05
Andrew Chander <taghead1@live.com.au>
2019-02-25 21:15:15 +1100

    Clarified comment in config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 class config:
     token = input("Discord Bot Token:")
     giphy_token = input("Giphy Token:")
-    #token = "NDg3NjE4MDY2MTcyMDg0MjI1.DnQfFQ.4ZW1XT0Q4uc_nf_AEeG8MfzGjIs" #Discord bot token
-    #giphy_token = "DT5O6VZzIN1FDrq6pjCIwFrltIS44v2n" #Giphy API token
+    #token = "NDg3NjE4MDY2MTcyMDg0MjI1.DnQfFQ.4ZW1XT0Q4uc_nf_AEeG8MfzGjIs" #Discord bot token example
+    #giphy_token = "DT5O6VZzIN1FDrq6pjCIwFrltIS44v2n" #Giphy API token example
     prefix = "@ZN " #Prefix
     sqlHost = "localhost" #SQL IP
     sqlUser = "normalUser" #SQL specified user
