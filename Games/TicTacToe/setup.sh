778bb58fd46416979a32bfdd1e17af1279b483a3
Andrew Chander <andrewchander@gmail.com>
2020-11-04 20:18:25 +1100

    Added skip environment variables and help

--- Games/TicTacToe/setup.sh
+++ Games/TicTacToe/setup.sh
@@ -1,7 +1,7 @@
-
 #!/bin/sh
 
 IGNORE_ROOT=0
+SKIP_CHECK_ENVIRONMENT_VARIABLES=0
 
 # Loop through arguments and process them
 for arg in "$@"
@@ -11,10 +11,19 @@ do
         IGNORE_ROOT=1
         shift
         ;;
-        -r|--root)
-        ROOT_DIRECTORY="$2"
-        shift # Remove argument name from processing
-        shift # Remove argument value from processing
+        -se|--skip-environment)
+        SKIP_CHECK_ENVIRONMENT_VARIABLES=1
+        echo "Skipping environment variables check"
+        shift
+        ;;
+        -h|--help)
+        echo """ARGUMENTS
+        -i  --ignore
+        -se --skip-environment
+        -h  --help
+        """
+        exit 0
+        shift
         ;;
         *)
         OTHER_ARGUMENTS+="$1"
@@ -38,76 +47,79 @@ fi
 
 cd ~
 
-# Environemnt variables required
-# DISCORDTOKEN 	- Contains discord bot api token
-# GIPHYTOKEN 	- Contains giphy api token
-# PREFIX        - Bot prefix
-# SQLHOST 	    - Contains SQL host ip
-# SQLUSER	    - Contains username for SQL Database
-# SQLPASS	    - Contains password for SQL Database
-# SQLDATABASE	- Contains database name
+if [ "$SKIP_CHECK_ENVIRONMENT_VARIABLES" -eq 0 ]
+then
+    # Environemnt variables required
+    # DISCORDTOKEN 	- Contains discord bot api token
+    # GIPHYTOKEN 	- Contains giphy api token
+    # PREFIX        - Bot prefix
+    # SQLHOST 	    - Contains SQL host ip
+    # SQLUSER	    - Contains username for SQL Database
+    # SQLPASS	    - Contains password for SQL Database
+    # SQLDATABASE	- Contains database name
 
-if [ -z ${DISCORDTOKEN+x} ]; 
-then 
-    echo "DISCORDTOKEN is unset... setting DISCORDTOKEN to default";
-    DISCORDTOKEN="input(\"Discord Bot Token:\")"; 
-else 
-    echo "DISCORDTOKEN is set to '$DISCORDTOKEN'";
-    DISCORDTOKEN="\"$DISCORDTOKEN"\"
-fi
+    if [ -z ${DISCORDTOKEN+x} ]; 
+    then 
+        echo "DISCORDTOKEN is unset... setting DISCORDTOKEN to default";
+        DISCORDTOKEN="input(\"Discord Bot Token:\")"; 
+    else 
+        echo "DISCORDTOKEN is set to '$DISCORDTOKEN'";
+        DISCORDTOKEN="\"$DISCORDTOKEN"\"
+    fi
 
-if [ -z ${GIPHYTOKEN+x} ]; 
-then 
-    echo "GIPHYTOKEN is unset... setting GIPHYTOKEN to default";
-    GIPHYTOKEN="input(\"Giphy Token:\")"; 
-else 
-    echo "GIPHYTOKEN is set to '$GIPHYTOKEN'";
-    GIPHYTOKEN="\"$GIPHYTOKEN"\"
-fi
+    if [ -z ${GIPHYTOKEN+x} ]; 
+    then 
+        echo "GIPHYTOKEN is unset... setting GIPHYTOKEN to default";
+        GIPHYTOKEN="input(\"Giphy Token:\")"; 
+    else 
+        echo "GIPHYTOKEN is set to '$GIPHYTOKEN'";
+        GIPHYTOKEN="\"$GIPHYTOKEN"\"
+    fi
 
-if [ -z ${PREFIX+x} ]; 
-then 
-    echo "PREFIX is unset... setting PREFIX to default";
-    PREFIX="input(\"Prefix:\")"; 
-else 
-    echo "PREFIX is set to '$PREFIX'";
-    PREFIX="\"$PREFIX"\"
-fi
+    if [ -z ${PREFIX+x} ]; 
+    then 
+        echo "PREFIX is unset... setting PREFIX to default";
+        PREFIX="input(\"Prefix:\")"; 
+    else 
+        echo "PREFIX is set to '$PREFIX'";
+        PREFIX="\"$PREFIX"\"
+    fi
 
-if [ -z ${SQLHOST+x} ]; 
-then 
-    echo "SQLHOST is unset... setting SQLHOST to default";
-    SQLHOST="input(\"My SQL server ip:\")"; 
-else 
-    echo "SQLHOST is set to '$SQLHOST'";
-    SQLHOST="\"$SQLHOST"\"
-fi
+    if [ -z ${SQLHOST+x} ]; 
+    then 
+        echo "SQLHOST is unset... setting SQLHOST to default";
+        SQLHOST="input(\"My SQL server ip:\")"; 
+    else 
+        echo "SQLHOST is set to '$SQLHOST'";
+        SQLHOST="\"$SQLHOST"\"
+    fi
 
-if [ -z ${SQLUSER+x} ]; 
-then 
-    echo "SQLUSER is unset... setting SQLUSER to default";
-    SQLUSER="input(\"My SQL database username:\")"; 
-else 
-    echo "SQLUSER is set to '$SQLUSER'";
-    SQLUSER="\"$SQLUSER"\"
-fi
+    if [ -z ${SQLUSER+x} ]; 
+    then 
+        echo "SQLUSER is unset... setting SQLUSER to default";
+        SQLUSER="input(\"My SQL database username:\")"; 
+    else 
+        echo "SQLUSER is set to '$SQLUSER'";
+        SQLUSER="\"$SQLUSER"\"
+    fi
 
-if [ -z ${SQLPASS+x} ]; 
-then 
-    echo "SQLPASS is unset... setting SQLPASS to default";
-    SQLPASS="input(\"My SQL users password:\")"; 
-else 
-    echo "SQLPASS is set to '$SQLPASS'";
-    SQLPASS="\"$SQLPASS"\"
-fi
+    if [ -z ${SQLPASS+x} ]; 
+    then 
+        echo "SQLPASS is unset... setting SQLPASS to default";
+        SQLPASS="input(\"My SQL users password:\")"; 
+    else 
+        echo "SQLPASS is set to '$SQLPASS'";
+        SQLPASS="\"$SQLPASS"\"
+    fi
 
-if [ -z ${SQLDATABASE+x} ]; 
-then 
-    echo "SQLDATABASE is unset... setting SQLDATABASE to default";
-    SQLDATABASE="input(\"My SQL database name:\")";  
-else 
-    echo "SQLDATABASE is set to '$SQLDATABASE'";
-    SQLDATABASE="\"$SQLDATABASE"\"
+    if [ -z ${SQLDATABASE+x} ]; 
+    then 
+        echo "SQLDATABASE is unset... setting SQLDATABASE to default";
+        SQLDATABASE="input(\"My SQL database name:\")";  
+    else 
+        echo "SQLDATABASE is set to '$SQLDATABASE'";
+        SQLDATABASE="\"$SQLDATABASE"\"
+    fi
 fi
 
 mkdir src
