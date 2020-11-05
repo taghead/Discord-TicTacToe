de416c191137aafdd855302440b8f9d087346a7c
Andrew Chander <andrewchander@gmail.com>
2020-11-05 14:15:52 +1100

    Added more help, made apply config into function

--- Games/TicTacToe/setup.sh
+++ Games/TicTacToe/setup.sh
@@ -1,65 +1,17 @@
 #!/bin/sh
 
+# Folder path variables
+PATH_TO_FILES="Games/TicTacToe"
+
+# Default variables
 IGNORE_ROOT=0
 SKIP_CHECK_ENVIRONMENT_VARIABLES=0
 SETUP_PATH_DIRECTORY="$HOME/TagheadDiscordBotCollection"
 START_AFTER_INSTALL=0
 
-# Loop through arguments and process them
-for arg in "$@"
-do
-    case "$arg" in
-        -ir|--ignore-root)
-            IGNORE_ROOT=1
-            echo " Ignoring root user status"
-            shift
-            ;;
-        -se|--skip-environment)
-            SKIP_CHECK_ENVIRONMENT_VARIABLES=1
-            echo "Skipping environment variables check"
-            shift
-            ;;
-        -p=*|--path=*)
-            SETUP_PATH_DIRECTORY="${arg#*=}"
-            echo "Setting up in ${arg#*=} as the path"
-            [ -d ${arg#*=} ] && echo "Path ${arg#*=} exists."
-            [ ! -d ${arg#*=} ] && echo "Path does not exists... making path ${arg#*=}" && mkdir ${arg#*=}
-            [ ! -d ${arg#*=} ] && echo "Unable to make directory ${arg#*=}" && exit 0
-            shift
-            ;;
-        -s|-start)
-            echo "Starting application after install"
-            START_AFTER_INSTALL=1
-            shift
-            ;;
-        -h|--help)
-            echo """ARGUMENTS
-            -ir --ignore-root       | Ignores user privileges check
-            -se --skip-environment  | Skips environment variables check
-            -p= --path=             | Set install path. Example -p=/opt/bot
-            -s  --start             | Start application after install
-            -h  --help              | Helps
-            """
-            exit 0
-            shift
-            ;;
-        *)
-            OTHER_ARGUMENTS+="$1"
-            shift # Remove generic argument from processing
-            ;;
-    esac
-done
-
-if [ "$(id -u)" -ne "0" -a "$IGNORE_ROOT" -eq 0 ];
-then
-    echo """
-    This script runs optimally as a root user. Please run as root or sudo.
-    If you wish to ignore this wait 10 seconds for the code to continue,
-    else press CTRL+C.
-    """
-    sleep 10
-fi
-
+# Functions
+applyconfig()
+{
 if [ "$SKIP_CHECK_ENVIRONMENT_VARIABLES" -eq 0 ]
 then
     # Environemnt variables required
@@ -135,15 +87,6 @@ then
     fi
 fi
 
-mkdir "$SETUP_PATH_DIRECTORY"
-cd "$SETUP_PATH_DIRECTORY"
-git init
-git remote add -f origin https://gitlab.com/Taghead/TagheadDiscordBotCollection.git
-git config core.sparseCheckout true
-echo "Games/TicTacToe" >> .git/info/sparse-checkout
-git pull origin master
-rm -R "$SETUP_PATH_DIRECTORY/.git"
-
 echo "
 class config:
     token = "$DISCORDTOKEN"
@@ -153,7 +96,96 @@ class config:
     sqlUser = "$SQLUSER"
     sqlPassword = "$SQLPASS" 
     sqlDatabase = "$SQLDATABASE" 
-" > Games/TicTacToe/config.py
+" > "$SETUP_PATH_DIRECTORY/$PATH_TO_FILES/config.py"
+}
+
+# Handle arguments
+for arg in "$@"
+do
+    case "$arg" in
+        -ir|--ignore-root)
+            IGNORE_ROOT=1
+            echo " Ignoring root user status"
+            shift
+            ;;
+        -se|--skip-environment)
+            SKIP_CHECK_ENVIRONMENT_VARIABLES=1
+            echo "Skipping environment variables check"
+            shift
+            ;;
+        -p=*|--path=*)
+            SETUP_PATH_DIRECTORY="${arg#*=}"
+            echo "Setting up in ${arg#*=} as the path"
+            [ -d ${arg#*=} ] && echo "Path ${arg#*=} exists."
+            [ ! -d ${arg#*=} ] && echo "Path does not exists... making path ${arg#*=}" && mkdir ${arg#*=}
+            [ ! -d ${arg#*=} ] && echo "Unable to make directory ${arg#*=}" && exit 0
+            shift
+            ;;
+        -s|--start)
+            echo "Starting application after install"
+            START_AFTER_INSTALL=1
+            shift
+            ;;
+        -snc|--start-new-config)
+            echo "Starting application after applying new configuration"
+            
+            shift
+            ;;
+        -h|--help)
+            echo -e """
+            \nEnvironment Variables
+                These variables are used to configure the config.py, not
+                declaring a variable will default it to an input prompt
+                on every run.
+                    DISCORDTOKEN 	- Contains discord bot api token
+                    GIPHYTOKEN 	- Contains giphy api token
+                    PREFIX        - Bot prefix
+                    SQLHOST 	    - Contains SQL host ip
+                    SQLUSER	    - Contains username for SQL Database
+                    SQLPASS	    - Contains password for SQL Database
+                    SQLDATABASE	- Contains database name
+
+            \nARGUMENTS
+                -ir     --ignore-root       | Ignores user privileges check
+                -se     --skip-environment  | Skips environment variables check
+                -p=     --path=             | Set install path. Example -p=/opt/bot
+                -s      --start             | Start application after install
+                -snc    --start-new-config  | Start applications and re-check environment
+                                            | variables for new values to apply to the
+                                            | configuration files.
+                                            
+                -h      --help              | Helps
+            """
+            exit 0
+            shift
+            ;;
+        *)
+            OTHER_ARGUMENTS+="$1"
+            shift # Remove generic argument from processing
+            ;;
+    esac
+done
+
+if [ "$(id -u)" -ne "0" -a "$IGNORE_ROOT" -eq 0 ];
+then
+    echo """
+    This script runs optimally as a root user. Please run as root or sudo.
+    If you wish to ignore this wait 10 seconds for the code to continue,
+    else press CTRL+C.
+    """
+    sleep 10
+fi
+
+mkdir "$SETUP_PATH_DIRECTORY"
+cd "$SETUP_PATH_DIRECTORY"
+git init
+git remote add -f origin https://gitlab.com/Taghead/TagheadDiscordBotCollection.git
+git config core.sparseCheckout true
+echo "$PATH_TO_FILES" >> .git/info/sparse-checkout
+git pull origin master
+rm -R "$SETUP_PATH_DIRECTORY/.git"
+
+applyconfig
 
 unset DISCORDTOKEN
 unset GIPHYTOKEN
@@ -163,7 +195,7 @@ unset SQLUSER
 unset SQLPASS
 unset SQLDATABASE
 
-cd Games/TicTacToe
+cd "$SETUP_PATH_DIRECTORY/$PATH_TO_FILES"
 
 apt-get install -y python3-pip python3-venv
 python3 -m venv env
